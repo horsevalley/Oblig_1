35f29b2982aadb0d47768c81352d0f5563d0d826
jonashestdahl <hestdahl@gmail.com>
2016-02-02 13:36:13 +0100

    Added Program class

---
+++ src/no/horsevalley/Program.java
@@ -0,0 +1,11 @@
+package no.horsevalley;
+
+/**
+ * Created by jonashestdahl on 02/02/16.
+ *
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello, git");
+    }
+}
